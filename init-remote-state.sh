325cc3cb8f33b54aa1623fca874f8ab0be9b0a2b
Stuart Izon <stuart.izon@thoughtworks.com>
2020-11-20 16:13:57 +1100

    Alter init remote state script

--- init-remote-state.sh
+++ init-remote-state.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [[ -z $(aws s3api head-bucket --bucket $STATE_BUCKET_NAME) ]]; then
+if [[ -n $(aws s3api head-bucket --bucket $STATE_BUCKET_NAME 2>&1 || true) ]]; then
     aws s3api create-bucket --bucket $STATE_BUCKET_NAME --region ap-southeast-1
 fi
