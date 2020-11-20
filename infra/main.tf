6bb8d508fd0fcc9ac701352854b32d634a749443
Stuart Izon <stuart.izon@thoughtworks.com>
2020-11-20 12:36:27 +1100

    Add initial infra

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 0.13.0"
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "ap-southeast-1"
+}
+
+data "aws_route53_zone" "root" {
+  name = var.domain_name
+}
+
+module "website" {
+  source      = "stuartizon/static-website/aws"
+  version     = "0.1.4"
+  domain_name = "www.${var.domain_name}"
+  redirects   = var.domain_name
+  zone_id     = data.aws_route53_zone.root.id
+}
---
+++ infra/variables.tf
@@ -0,0 +1,4 @@
+variable "domain_name" {
+  type        = string
+  description = "Top level domain name which should exist as a Route 53 zone"
+}
